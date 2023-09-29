f2c40f4f7794a1757384e4d7b8de7149de8a7902
brandon-debruyn <D20123654@mytudublin.ie>
2023-09-29 18:17:31 +0100

    lect updates

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -1,4 +1,11 @@
 extends CharacterBody3D
 
+@export var move_speed:float=5
+@export var rot_speed:float=1
+
 func _process(delta):	
-		translate(Vector3.FORWARD * delta)
+	var move = Input.get_axis("move_backward", "move_forward")
+	print(move)
+	translate(Vector3.FORWARD * move * delta * move_speed)
+	var turn = Input.get_axis("move_left", "move_right")
+	rotate(Vector3.UP, turn * delta * rot_speed)
